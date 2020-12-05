8297952d21389beb6e0eaf9f86c212538c7556e8
Jakob Smith <61601527+jakobtsmith@users.noreply.github.com>
2020-12-04 22:04:14 -0600

    Added comments

--- musiclib/__init__.py
+++ musiclib/__init__.py
@@ -5,59 +5,34 @@ from flask_login import LoginManager
 import secrets as secret
 import os
 
+#Create the flask environment
 app = Flask(__name__)
 
+#Set the secret key for the current running app
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+#create the mysql environment
 mysql = MySQL()
 
-
+#set up the mysql information to pull from the required database using user information
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = secret.USER
 app.config['MYSQL_DATABASE_PASSWORD'] = secret.PASS
 app.config['MYSQL_DATABASE_DB'] = 'jts0270'
 mysql.init_app(app)
 
+#set up bcrypt to hash passwords
 bcrypt = Bcrypt(app)
+
+#create the mysql database connection
 conn = mysql.connect()
+
+#setup the login manager
 loginManager = LoginManager()
 loginManager.login_view = 'Login'   
 loginManager.login_message_category = 'info'
 loginManager.init_app(app)
 
-
+#bring in the route handler
 from musiclib import routes
-
-
-
-'''
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    conn = mysql.connect()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Artist")
-    data = []
-    for item in cur:
-        data.append(item)
-        print(item)
-    conn.close()
-    return str(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
--- musiclib/models.py
+++ musiclib/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from musiclib import mysql
 from musiclib import loginManager
 
+#User loader function for loginmanager. Returns all info of the current user
 @loginManager.user_loader
 def load_user(ID):
     cur = mysql.connect().cursor()
@@ -9,6 +10,7 @@ def load_user(ID):
     result=cur.fetchone()
     return MyUser(result[0], result[1], result[2], result[3])
 
+#User definition for the login manager. Assists with authorization and authentication
 class MyUser(UserMixin):
     def __init__(self, ID, username, password, email, active=True):
         self.ID = ID
